9b855555b030cb6f63be3e1595d91dfb199024e6
Adam Naylor <anaylor@Development.HealthyChoices.gsi.gov.uk>
2017-11-14 11:47:07 +0000

    Adds dockerfile and related changes

---
+++ NHS111.Business.CCG.Api/start.ps1
@@ -0,0 +1,13 @@
+﻿$jsonPath = "$env:APPLICATION_PATH\appsettings.json"
+
+Write-Output "Loading json file from $jsonPath"
+
+$json = Get-Content $jsonPath -raw | ConvertFrom-Json
+
+$json.connectionstring=[Environment]::GetEnvironmentVariable("connectionstring")
+$json.tablereference=[Environment]::GetEnvironmentVariable("tablereference")
+$json.NhsPathwaysConfigurations.ApplicationUrl=[Environment]::GetEnvironmentVariable("NhsPathwaysConfigurations.ApplicationUrl")
+
+$json | ConvertTo-Json  | Set-Content $jsonPath
+
+dotnet ./NHS111.Business.CCG.Api.dll
