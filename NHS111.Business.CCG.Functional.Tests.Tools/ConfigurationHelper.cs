0c53c6f50c109864a250f420bd69ade7ebbe038d
Patrick Hughes <patrick.hughes1@nhs.net>
2018-07-09 17:03:30 +0100

    added initial search distance tests

---
+++ NHS111.Business.CCG.Functional.Tests.Tools/ConfigurationHelper.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
+namespace NHS111.Business.CCG.Functional.Tests.Tools
+{
+    public class ConfigurationHelper
+    {
+        public static IConfigurationRoot GetIConfigurationRoot(string outputPath)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(outputPath)
+                .AddJsonFile("appsettings.json")
+                    .AddJsonFile("C:\\Configurations\\nhs111-shared-resources\\appsettings.debug.json", optional: true)
+                .Build();
+        }
+    }
+}
