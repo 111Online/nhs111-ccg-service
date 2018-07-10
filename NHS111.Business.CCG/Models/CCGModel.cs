73f91d178248f744c1e02bb75cd962e16382958c
Patrick Hughes <patrick.hughes1@nhs.net>
2018-07-10 12:04:28 +0100

    added search distance functional tests

--- NHS111.Business.CCG/Models/CCGModel.cs
+++ NHS111.Business.CCG/Models/CCGModel.cs
@@ -1,10 +1,12 @@
-﻿namespace NHS111.Business.CCG.Models {
+﻿using Newtonsoft.Json;
+
+namespace NHS111.Business.CCG.Models {
 
     public class CCGModel {
         public string Postcode { get; set; }
         public string CCG { get; set; }
         public string App { get; set; }
-
+        [JsonProperty(PropertyName = "dosSearchDistance")]
         public string DOSSearchDistance { get; set; }
     }
 }
