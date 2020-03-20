e46d6107371f6b27fedd356109052f883aa986cb
Andy Smith <andrew.smith60@nhs.net>
2020-03-20 10:18:54 +0000

    Reverting to previous version to stop early changes in master

--- NHS111.DataImport.CCG/Program.cs
+++ NHS111.DataImport.CCG/Program.cs
@@ -1,181 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
-
+using System.Threading.Tasks.Dataflow;
+using System.Timers;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
-
+using NHS111.Domain.CCG;
 using CsvHelper;
-
+using CsvHelper.TypeConversion;
 using Microsoft.WindowsAzure.Storage.Table;
 using NHS111.Domain.CCG.Models;
+using Nito.AsyncEx;
+using Nito.AsyncEx.Synchronous;
+using CsvHelper.Configuration;
 
 using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing;
 
 namespace NHS111.DataImport.CCG
 {
-    public class Program
+    class Program
     {
-        private static string _accountName;
+        private static string _accountname;
         private static string _tableReference;
-        private static string _ccgTableReference;
+        private static string _ccgtableReference;
         private static string _accountKey;
         private static string _postcodeCsvFilePath;
         private static string _ccgCsvFilePath;
-        private static string _nationalWhitelistFilePath;
         private static string _dosSearchDistanceFilePath;
         private static int _counter;
         private static int _recordCount;
-        private static readonly Regex RegexRemoveWhitespace = new Regex(@"\s+");
+        private static readonly Regex _regexRemoveWhitespace = new Regex(@"\s+");
         private static int _terminatedPostcodesCount;
         private static Dictionary<string, int> _dosSearchDistanceLookup = new Dictionary<string, int>();
         private static Dictionary<string, int> _dosSearchDistancePartialLookup = new Dictionary<string, int>();
-        private static bool _onlyImportSTPData = false;
+        private static bool _onlyImportstpData = false;
         private static Dictionary<string, PostcodeRecord> _ccgLookup = new Dictionary<string, PostcodeRecord>();
-        
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+
             Console.WriteLine("Beginning Data import");
-            
             LoadSettings(args);
 
             var clock = new Stopwatch();
-            
             clock.Start();
-
-            var nationalWhiteList = new List<string>();
-
-            if (!string.IsNullOrWhiteSpace(_nationalWhitelistFilePath))
-            {
-                nationalWhiteList = LoadNationalWhitelist(_nationalWhitelistFilePath);
-            }
-
-            LoadCCGLookupData(_ccgCsvFilePath, nationalWhiteList).Wait();
-
+            LoadCCGLookupdata(_ccgCsvFilePath).Wait();
             LoadDOSSearchDistanceLookupdata(_dosSearchDistanceFilePath).Wait();
 
-            if (!_onlyImportSTPData)
+            if (!_onlyImportstpData)
             {
                 RunImport().Wait();
                 clock.Stop();
-                Console.WriteLine("finished importing " + _recordCount + " in " + TimeSpan.FromMilliseconds(clock.ElapsedMilliseconds).ToString(@"hh\:mm\:ss"));
+                Console.WriteLine("finished importing " + _recordCount + " in " +
+                                  TimeSpan.FromMilliseconds(clock.ElapsedMilliseconds).ToString(@"hh\:mm\:ss"));
             }
-
             clock.Stop();
-
-            Console.WriteLine("finished importing stp data only in " + TimeSpan.FromMilliseconds(clock.ElapsedMilliseconds).ToString(@"hh\:mm\:ss"));
+            Console.WriteLine("finished importing stp data only in " +
+                              TimeSpan.FromMilliseconds(clock.ElapsedMilliseconds).ToString(@"hh\:mm\:ss"));
             Console.ReadLine();
+
         }
 
-        public static List<string> LoadNationalWhitelist(string path)
+        public static async Task LoadCCGLookupdata(string csvFilePath)
         {
-            var whitelist = new List<string>();
-
-            try
-            {
-                using (var sr = new StreamReader(path))
-                {
-                    var content = sr.ReadToEnd();
-
-                    whitelist.AddRange(content.Split('|'));
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(string.Format("Unable to load the national whitelist file at path: {0} Error {1}", path, e.Message));
-            }
-
-            return whitelist;
-        }
-
-        public static async Task LoadCCGLookupData(string csvFilePath, List<string> nationalWhitelist)
-        {
-            var storageAccount = new CloudStorageAccount(new StorageCredentials(_accountName, _accountKey), true);
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(_accountname, _accountKey), true);
             var tableClient = storageAccount.CreateCloudTableClient();
-            var stpTable = tableClient.GetTableReference(_ccgTableReference);
-            var csvLookup = new CsvReader(new StreamReader(csvFilePath));
-            csvLookup.Read();
-            csvLookup.ReadHeader();
+            var stptable = tableClient.GetTableReference(_ccgtableReference);
+            var csvlookup = new CsvReader(new StreamReader(csvFilePath));
+            csvlookup.Read();
+            csvlookup.ReadHeader();
+            var ccgs = new List<STPEntity>();
             var batch = new TableBatchOperation();
 
-            while (csvLookup.Read())
+            while (csvlookup.Read())
             {
-                var pharmacyServiceIdWhitelist = csvLookup.GetField<string>("PharmacyReferralServiceIdWhitelist");
-
-                if (!string.IsNullOrWhiteSpace(pharmacyServiceIdWhitelist))
+                batch.Add(TableOperation.InsertOrReplace(new STPEntity()
                 {
-                    var whitelistCollection = pharmacyServiceIdWhitelist
-                        .Split('|')
-                        .ToList();
+                    PartitionKey = "CCGs",
+                    RowKey = csvlookup.GetField<string>("CCG16CD"),
+                    CCGId = csvlookup.GetField<string>("CCG16CD"),
+                    STPId = csvlookup.GetField<string>("STP17CD"),
+                    STPName = csvlookup.GetField<string>("STP17NM"),
+                    CCGName = csvlookup.GetField<string>("CCG16NM"),
+                    ProductName = csvlookup.GetField<string>("Product"),
+                    LiveDate = csvlookup.GetField<DateTime?>("LiveDate", new DateTimeLocalConverter()),
+                    PharmacyServiceIdWhitelist = csvlookup.GetField<string>("PharmacyReferralServiceIdWhitelist"),
+                    ReferralServiceIdWhitelist = csvlookup.GetField<string>("ReferralServiceIdWhitelist")
+                }));
 
-                    whitelistCollection.AddRange(nationalWhitelist);
-                    pharmacyServiceIdWhitelist = string.Join('|', whitelistCollection);
-                }
-
-                batch.Add(
-                    TableOperation.InsertOrReplace(
-                        new STPEntity
-                            {
-                                PartitionKey = "CCGs",
-                                RowKey = csvLookup.GetField<string>("CCG16CD"),
-                                CCGId = csvLookup.GetField<string>("CCG16CD"),
-                                STPId = csvLookup.GetField<string>("STP17CD"),
-                                STPName = csvLookup.GetField<string>("STP17NM"),
-                                CCGName = csvLookup.GetField<string>("CCG16NM"),
-                                ProductName = csvLookup.GetField<string>("Product"),
-                                LiveDate = csvLookup.GetField<DateTime?>("LiveDate", new DateTimeLocalConverter()),
-                                PharmacyServiceIdWhitelist = pharmacyServiceIdWhitelist,
-                                ReferralServiceIdWhitelist = csvLookup.GetField<string>("ReferralServiceIdWhitelist")
-                            }));
-
-                if (!_ccgLookup.ContainsKey(csvLookup.GetField<string>("CCG16CD")))
+                if (!_ccgLookup.ContainsKey(csvlookup.GetField<string>("CCG16CD")))
                 {
-                    _ccgLookup.Add(
-                        csvLookup.GetField<string>("CCG16CD"),
-                        new PostcodeRecord
-                            {
-                                AppName = csvLookup.GetField<string>("Product"),
-                                StpName = csvLookup.GetField<string>("STP17NM"),
-                                CcgName = csvLookup.GetField<string>("CCG16NM"),
-                                CCGId = csvLookup.GetField<string>("CCG16CD")
-                            });
+                    _ccgLookup.Add(csvlookup.GetField<string>("CCG16CD"), new PostcodeRecord()
+                    {
+                        AppName = csvlookup.GetField<string>("Product"),
+                        StpName = csvlookup.GetField<string>("STP17NM"),
+                        CcgName = csvlookup.GetField<string>("CCG16NM"),
+                        CCGId = csvlookup.GetField<string>("CCG16CD")
+                    });
                 }
-
                 if (batch.Count % 100 == 0)
                 {
-                    await stpTable.ExecuteBatchAsync(batch);
+                    await stptable.ExecuteBatchAsync(batch);
                     batch = new TableBatchOperation();
                 }
             }
+            if (batch.Count > 0) await stptable.ExecuteBatchAsync(batch);
 
-            if (batch.Count > 0)
-            {
-                await stpTable.ExecuteBatchAsync(batch);
-            }
         }
-        
+
+
         public static async Task LoadDOSSearchDistanceLookupdata(string xlsFilePath)
         {
             var package = new ExcelPackage(new FileInfo(xlsFilePath));
-            
             Console.WriteLine("Loading DOS search distance Data");
-            
             var fullPostcodeSheet = package.Workbook.Worksheets[2];
-            
-            for (var i = 1; i <= fullPostcodeSheet.Dimension.End.Row; i++)
+            for (int i = 1; i <= fullPostcodeSheet.Dimension.End.Row; i++)
             {
                 _dosSearchDistanceLookup.Add(NormalisePostcode(fullPostcodeSheet.Cells[i, 1].Value.ToString()), Convert.ToInt32(fullPostcodeSheet.Cells[i, 2].Value));
             }
 
             var partialPostcodeSheet = package.Workbook.Worksheets[1];
-            
-            for (var i = 2; i <= partialPostcodeSheet.Dimension.End.Row; i++)
+            for (int i = 2; i <= partialPostcodeSheet.Dimension.End.Row; i++)
             {
-                _dosSearchDistancePartialLookup.Add(RegexRemoveWhitespace.Replace(partialPostcodeSheet.Cells[i, 1].Value.ToString(), string.Empty), Convert.ToInt32(partialPostcodeSheet.Cells[i, 2].Value));
+                _dosSearchDistancePartialLookup.Add(_regexRemoveWhitespace.Replace(partialPostcodeSheet.Cells[i, 1].Value.ToString(), string.Empty), Convert.ToInt32(partialPostcodeSheet.Cells[i, 2].Value));
             }
 
             Console.WriteLine("Finished loading DOS search distance Data");
@@ -183,11 +139,11 @@ namespace NHS111.DataImport.CCG
 
         public static async Task  RunImport()
         {
-            var storageAccount = new CloudStorageAccount(new StorageCredentials(_accountName, _accountKey), true);
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(_accountname, _accountKey), true);
             var tableClient = storageAccount.CreateCloudTableClient();
             var table = tableClient.GetTableReference(_tableReference);
             var csv = new CsvReader(new StreamReader(_postcodeCsvFilePath));
-            var batchSizeMax = 100;
+            var batchsizeMax = 100;
             var batch = new TableBatchOperation();
             _terminatedPostcodesCount = 0;
             var i = 0;
@@ -196,36 +152,29 @@ namespace NHS111.DataImport.CCG
             csv.ReadHeader();
             _recordCount = File.ReadLines(_postcodeCsvFilePath).Count() -1;
             var tasks = new List<Task>();
-            
             while (csv.Read())
             {
-                csv.TryGetField<string>("doterm", out var terminatedDate);
+                var termenatedDate = "";
+                csv.TryGetField<string>("doterm", out termenatedDate);
 
-                if (string.IsNullOrWhiteSpace(terminatedDate))
+                if (String.IsNullOrWhiteSpace(termenatedDate))
                 {
-                    var dosSearchDistance = string.Empty;
-
-                    csv.TryGetField<string>("ccg", out var ccgId);
-                    csv.TryGetField<string>("pcd", out var postcode);
-                    csv.TryGetField<string>("pcds", out var formattedPostcode);
-
-                    var partialPostcode = formattedPostcode
-                        .Split(' ')
-                        .First()
-                        .Trim();
-
+                    var ccgName = "";
+                    var ccgId = "";
+                    var postcode = "";
+                    var formattedPostcode = "";
+                    var partialPostcode = "";
+                    var app = "";
+                    var dosSearchDistance = "";
+                    csv.TryGetField<string>("ccg", out ccgId);
+                    csv.TryGetField<string>("pcd", out postcode);
+                    csv.TryGetField<string>("pcds", out formattedPostcode);
+                    partialPostcode = formattedPostcode.Split(' ').First().Trim();
                     if (_dosSearchDistanceLookup.ContainsKey(NormalisePostcode(postcode)))
-                    {
                         dosSearchDistance = _dosSearchDistanceLookup[NormalisePostcode(postcode)].ToString();
-                    }
                     else if (_dosSearchDistancePartialLookup.ContainsKey(partialPostcode))
-                    {
                         dosSearchDistance = _dosSearchDistancePartialLookup[partialPostcode].ToString();
-                    }
-                    else
-                    {
-                        noDosSearchDistanceCount++;
-                    }
+                    else noDosSearchDistanceCount++;
                   
                     batch.Add(TableOperation.InsertOrReplace(new CCGEntity()
                     {
@@ -237,16 +186,15 @@ namespace NHS111.DataImport.CCG
                         RowKey = NormalisePostcode(postcode),
                         DOSSearchDistance = dosSearchDistance
                     }));
-
                     i++;
 
-                    if (i % batchSizeMax == 0)
+                    if ((i % batchsizeMax) == 0)
                     {
-                        var task = ImportBatch(table, batch);
+
+                        var task = ImportBatch(table, batch, i);
                         tasks.Add(task);
                         batch = new TableBatchOperation();
                     }
-                    
                     if (tasks.Count == 25) // limit async tasks to 10
                     {
                         await Task.WhenAll(tasks);
@@ -256,36 +204,37 @@ namespace NHS111.DataImport.CCG
                 else _terminatedPostcodesCount++;
             }
             //run remaining records
-            tasks.Add(ImportBatch(table, batch));
+            tasks.Add(ImportBatch(table, batch, i));
             await Task.WhenAll(tasks);
             Console.WriteLine("DOS Search distance not mapped count: " + noDosSearchDistanceCount);
         }
 
-        public static async Task ImportBatch(CloudTable table, TableBatchOperation batch)
+        public static async Task ImportBatch(CloudTable table, TableBatchOperation batch, int number)
         {
             var importedCount = await table.ExecuteBatchAsync(batch);
-            var newCount = _counter + importedCount.Count;
-            _counter = newCount;
-            Console.WriteLine("Imported " + _counter + " records ("+_terminatedPostcodesCount +" terminated) of " + _recordCount +  " (" + CalculatePercentDone() + "%)");
+            var newcount = _counter + importedCount.Count;
+            _counter = newcount;
+            Console.WriteLine("Imported " + _counter + " records ("+_terminatedPostcodesCount +" terminated) of " + _recordCount +  " (" + CalcuatePercentDone() + "%)");
         }
 
-        public static string CalculatePercentDone()
+        public static string CalcuatePercentDone()
         {
-            return ((_counter + (decimal) _terminatedPostcodesCount) / _recordCount * 100m).ToString("0.00");
+            return ((((decimal) _counter + (decimal) _terminatedPostcodesCount)
+                     / (decimal) _recordCount) * 100m).ToString("0.00");
         }
 
         private static string NormalisePostcode(string postcode)
         {
-            return RegexRemoveWhitespace.Replace(postcode, string.Empty).ToUpper();
+            return _regexRemoveWhitespace.Replace(postcode, string.Empty).ToUpper();
         }
 
         public static void LoadSettings(string[] args)
         {
-            for (var i = 0; i < args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 if (args[i].StartsWith("-AccountName="))
                 {
-                    _accountName = args[i].Replace("-AccountName=", "");
+                    _accountname = args[i].Replace("-AccountName=", "");
                 }
 
                 if (args[i].StartsWith("-TableRef="))
@@ -295,7 +244,7 @@ namespace NHS111.DataImport.CCG
 
                 if (args[i].StartsWith("-ccgTableRef="))
                 {
-                    _ccgTableReference = args[i].Replace("-ccgTableRef=", "");
+                    _ccgtableReference = args[i].Replace("-ccgTableRef=", "");
                 }
 
                 if (args[i].StartsWith("-ccgCsvFilePath="))
@@ -307,15 +256,9 @@ namespace NHS111.DataImport.CCG
                 {
                     _accountKey = args[i].Replace("-AccountKey=","");
                 }
-                
                 if (args[i].StartsWith("-CSVFilePath="))
                 {
                     _postcodeCsvFilePath = args[i].Replace("-CSVFilePath=","");
-                }
-
-                if (args[i].StartsWith("-whitelistFilePath="))
-                {
-                    _nationalWhitelistFilePath = args[i].Replace("-whitelistFilePath=","");
                 }
 
                 if (args[i].StartsWith("-DosSaerchDistanceFilePath"))
@@ -325,9 +268,37 @@ namespace NHS111.DataImport.CCG
 
                 if (args[i].StartsWith("-STPDataOnly"))
                 {
-                    _onlyImportSTPData = true;
+                    _onlyImportstpData = true;
                 }
             }
+
+
         }
+
+
+    }
+
+    public class DateTimeLocalConverter : DateTimeConverter, ITypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+                return null;
+            return DateTime.ParseExact(text, "dd/MM/yyyy", null);
+        }
+    }
+
+    public class PostcodeRecord
+    {
+        public string CCGId { get; set; }
+        public string StpName { get; set; }
+        public string CcgName { get; set; }
+        public string AppName { get; set; }
+    }
+
+    public class DosSearchRecord
+    {
+        public string Postcode { get; set; }
+        public int searchDistance { get; set; }
     }
 }
