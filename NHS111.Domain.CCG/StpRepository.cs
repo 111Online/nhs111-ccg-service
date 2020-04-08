d49cb20fc5574b2e7d32ac8bc08d9ce1b9ec5cd6
Sebastian <11445087+sebader@users.noreply.github.com>
2020-04-08 10:37:37 +0200

    load STP entities into memory on startup

--- NHS111.Domain.CCG/StpRepository.cs
+++ NHS111.Domain.CCG/StpRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAzure.Storage.Table;
 using NHS111.Domain.CCG.Models;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NHS111.Domain.CCG
@@ -9,6 +10,8 @@ namespace NHS111.Domain.CCG
     public class STPRepository : ISTPRepository
     {
         private readonly CloudTable _table;
+        private List<STPEntity> allEntities = new List<STPEntity>();
+
 
         public STPRepository(IAzureAccountSettings settings)
         {
@@ -17,18 +20,12 @@ namespace NHS111.Domain.CCG
             _table = tableClient.GetTableReference(settings.STPTableReference);
 
             _table.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+
+            // Preload all STP entities into memory on startup
+            allEntities = LoadEntitiesIntoMemory().GetAwaiter().GetResult();
         }
 
-        public async Task<STPEntity> Get(string ccgId)
-        {
-            var operation = TableOperation.Retrieve<STPEntity>("CCG", ccgId);
-
-            var result = await _table.ExecuteAsync(operation);
-
-            return result.Result as STPEntity;
-        }
-
-        public async Task<List<STPEntity>> List()
+        private async Task<List<STPEntity>> LoadEntitiesIntoMemory()
         {
             TableContinuationToken token = null;
 
@@ -41,6 +38,37 @@ namespace NHS111.Domain.CCG
             } while (token != null);
 
             return entities;
+        }
+
+        public async Task<STPEntity> Get(string ccgId)
+        {
+            return allEntities.FirstOrDefault(e => e.CCGId == ccgId);
+            /*
+            var operation = TableOperation.Retrieve<STPEntity>("CCG", ccgId);
+
+            var result = await _table.ExecuteAsync(operation);
+
+            return result.Result as STPEntity;
+            */
+        }
+
+        public async Task<List<STPEntity>> List()
+        {
+            return allEntities;
+
+            /*
+            TableContinuationToken token = null;
+
+            var entities = new List<STPEntity>();
+            do
+            {
+                var queryResult = await _table.ExecuteQuerySegmentedAsync(new TableQuery<STPEntity>(), token);
+                entities.AddRange(queryResult.Results);
+                token = queryResult.ContinuationToken;
+            } while (token != null);
+
+            return entities;
+            */
         }
     }
 }
